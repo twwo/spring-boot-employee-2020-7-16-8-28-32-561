95cc26e3b1312034fe0e2127e6f5ba5444ead896
Zeeeeeeju <QUENTIN.LIANG@OOCL.COM>
2020-07-28 21:45:48 +0800

    [RESTful API Comprehensive Exercise][Quentin] feature: add EmployeeController class

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    
+
+}
