84edd329a85f30c0fc573798099f2feac5a798b1
Zeeeeeeju <QUENTIN.LIANG@OOCL.COM>
2020-07-29 17:01:27 +0800

    [RESTful API Comprehensive Exercise][Quentin & Shao] refactor: modify all method use mock

--- src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
@@ -6,30 +6,40 @@ import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class EmployeeServiceTest {
 
-    private final EmployeeRepository employeeRepository = new EmployeeRepository();
+    private final EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
     private final EmployeeService service = new EmployeeService(employeeRepository);
 
     @Test
     void should_return_all_employees_when_get_all_given_none() {
         //given
+        given(employeeRepository.getAll()).willReturn(new ArrayList<>(Arrays.asList(
+                new Employee(4, "alibaba1", 20, "male", 6000),
+                new Employee(1, "Quentin", 18, "male", 10000)
+        )));
 
         //when
         List<Employee> employees = service.getAll();
 
         //then
         assertNotNull(employees);
+        assertEquals(2, employees.size());
     }
 
     @Test
     void should_return_employee_when_get_by_id_given_id() {
         //given
         Integer id = 1;
+        given(employeeRepository.getEmployeeById(id)).willReturn(new Employee(1, "Quentin", 18, "male", 10000));
 
         //when
         Employee employee = service.getEmployeeById(id);
@@ -43,6 +53,7 @@ public class EmployeeServiceTest {
     void should_return_employee_when_add_employee_given_employee() {
         //given
         Employee employee = new Employee(80, "ggggggg", 20, "male", 100);
+        given(employeeRepository.addEmployee(employee)).willReturn(employee);
 
         //when
         Employee addedEmployee = service.addEmployee(employee);
@@ -56,6 +67,7 @@ public class EmployeeServiceTest {
     void should_return_updated_employee_when_update_given_employee() {
         //given
         Employee employee = new Employee(1, "HHHHHHH", 30, "female", 200);
+        given(employeeRepository.getEmployeeById(employee.getId())).willReturn(new Employee(1, "Quentin", 18, "male", 10000));
 
         //when
         Employee updatedEmployee = service.updateEmployee(employee);
@@ -73,6 +85,7 @@ public class EmployeeServiceTest {
     void should_return_deleted_employee_when_delete_given_id() {
         //given
         Integer id = 1;
+        given(employeeRepository.getEmployeeById(id)).willReturn(new Employee(1, "Quentin", 18, "male", 10000));
 
         //when
         Employee employee = service.deleteEmployee(id);
@@ -87,12 +100,20 @@ public class EmployeeServiceTest {
         //given
         int page = 1;
         int pageSize = 5;
+        given(employeeRepository.findEmployeesByPage(page, pageSize)).willReturn(Arrays.asList(
+                new Employee(1, "Quentin", 18, "male", 10000),
+                new Employee(2, "Quentin", 18, "male", 10000),
+                new Employee(3, "Quentin", 18, "male", 10000),
+                new Employee(4, "Quentin", 18, "male", 10000),
+                new Employee(5, "Quentin", 18, "male", 10000)
+        ));
 
         //when
         List<Employee> employees = service.getEmployeesByPage(page, pageSize);
 
         //then
         assertNotNull(employees);
+
         assertEquals(5, employees.size());
     }
 
@@ -100,12 +121,18 @@ public class EmployeeServiceTest {
     void should_return_employees_when_query_by_gender_given_gender_is_male() {
         //given
         String gender = "male";
-
+        given(employeeRepository.getEmployeesByGender(gender)).willReturn(Arrays.asList(
+                new Employee(1, "Quentin", 18, "male", 10000),
+                new Employee(2, "Quentin", 18, "male", 10000),
+                new Employee(3, "Quentin", 18, "male", 10000),
+                new Employee(4, "Quentin", 18, "male", 10000),
+                new Employee(5, "Quentin", 18, "male", 10000)
+        ));
         //when
         List<Employee> employees = service.getEmployeesByGender(gender);
 
         //then
         assertNotNull(employees);
-        assertEquals(4, employees.size());
+        assertEquals(5, employees.size());
     }
 }
