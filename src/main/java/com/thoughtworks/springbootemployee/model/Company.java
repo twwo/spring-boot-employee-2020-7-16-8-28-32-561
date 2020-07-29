33b36a7694069a0c7d327acd522f34e2d9ee6cab
LISH5 <727904507@qq.com>
2020-07-29 19:32:26 +0800

    [RESTFUL API][ShaoLi] refactor: add @GeneratedValue

--- src/main/java/com/thoughtworks/springbootemployee/model/Company.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Company {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String companyName;
     private Integer employeesNumber;
