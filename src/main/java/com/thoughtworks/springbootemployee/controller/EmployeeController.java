2f052a001eab600da84efc637491b1422eb85202
Zeeeeeeju <QUENTIN.LIANG@OOCL.COM>
2020-07-29 17:39:26 +0800

    [RESTful API Comprehensive Exercise][Quentin & Shao] refactor: add params

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,56 +15,43 @@ import java.util.stream.Collectors;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    List<Employee> employeesData = new ArrayList<>(Arrays.asList(
-            new Employee(4, "alibaba1", 20, "male", 6000),
-            new Employee(11, "tengxun2", 19, "female", 7000),
-            new Employee(6, "alibaba3", 19, "male", 8000),
-            new Employee(13, "huawei", 60, "male", 4000),
-            new Employee(1, "Quentin", 18, "male", 10000),
-            new Employee(5, "goodboy", 70, "remale", 5000)
-    ));
+    private final EmployeeService service = new EmployeeService(new EmployeeRepository());
 
-    @GetMapping
-    public List<Employee> getAll(@RequestParam(value = "page", required = false) Integer page
-            , @RequestParam(value = "pageSize", required = false) Integer pageSize
-            , @RequestParam(value = "gender", required = false) String gender) {
-        if (page != null && pageSize != null)
-            return employeesData.stream().skip((page - 1) * pageSize).limit(pageSize).collect(Collectors.toList());
-        if (gender != null)
-            return employeesData.stream().filter(employee -> employee.getGender().equals(gender)).collect(Collectors.toList());
-        return employeesData;
+    @GetMapping()
+    public List<Employee> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping(params = {"page", "pageSize"})
+    public List<Employee> getAllByPage(Integer page, Integer pageSize) {
+        return service.getEmployeesByPage(page, pageSize);
+    }
+
+    @GetMapping(params = {"gender"})
+    public List<Employee> getAllByGender(String gender) {
+        return service.getEmployeesByGender(gender);
     }
 
     @GetMapping("/{employeeID}")
     public Employee getEmployeeByID(@PathVariable Integer employeeID) {
-        return employeesData.stream().filter(employee -> employee.getId() == employeeID).findFirst().get();
+        return service.getEmployeeById(employeeID);
     }
 
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public Employee addEmployee(@RequestBody Employee employee) {
-        employeesData.add(employee);
-        return employee;
+        return service.addEmployee(employee);
     }
 
     @PutMapping("/{employeeID}")
     public Employee modifyEmployee(@RequestBody Employee modifiedEmployee, @PathVariable Integer employeeID) {
-        for (Employee employee : employeesData) {
-            if (employee.getId().equals(employeeID)) {
-                employee.setName(modifiedEmployee.getName());
-                employee.setAge(modifiedEmployee.getAge());
-                employee.setGender(modifiedEmployee.getGender());
-                employee.setSalary(modifiedEmployee.getSalary());
-                return employee;
-            }
-        }
-        return null;
+        return service.updateEmployee(employeeID, modifiedEmployee);
     }
 
     @DeleteMapping("/{employeeID}")
     @ResponseStatus(value = HttpStatus.ACCEPTED)
-    public void deleteEmployee(@PathVariable Integer employeeID) {
-        employeesData.removeIf(employee -> employee.getId().equals(employeeID));
+    public Employee deleteEmployee(@PathVariable Integer employeeID) {
+        return service.deleteEmployee(employeeID);
     }
 
 }
