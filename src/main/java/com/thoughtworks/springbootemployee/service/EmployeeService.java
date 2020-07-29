d2edf48d51fb7e10c915ec04fce00f6962a4f79a
Zeeeeeeju <QUENTIN.LIANG@OOCL.COM>
2020-07-29 14:33:42 +0800

    [RESTful API Comprehensive Exercise][Quentin & Shao] feature: implement should_return_all_employees_when_get_all_given_none test

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -1,13 +1,20 @@
 package com.thoughtworks.springbootemployee.service;
 
 import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
     public List<Employee> getAll() {
-        return null;
+        return employeeRepository.getAll();
     }
 }
--- src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/EmployeeServiceTest.java
@@ -1,20 +1,29 @@
 package com.thoughtworks.springbootemployee;
 
 import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class EmployeeServiceTest {
 
     @Test
     void should_return_all_employees_when_get_all_given_none() {
         //given
-        EmployeeService service = new EmployeeService();
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        EmployeeService service = new EmployeeService(employeeRepository);
+        given(employeeRepository.getAll()).willReturn(new ArrayList<>());
 
         //when
         List<Employee> employees = service.getAll();
