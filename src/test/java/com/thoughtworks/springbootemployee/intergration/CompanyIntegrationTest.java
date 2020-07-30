1d3ee0118d521841c64632ee4ea28ca75ed7991a
LISH5 <727904507@qq.com>
2020-07-30 16:44:57 +0800

    [RESTFUL API][ShaoLi,Zach] test: add should_return_companies__when_hit_get_all_companies_given_nothing

---
+++ src/test/java/com/thoughtworks/springbootemployee/intergration/CompanyIntegrationTest.java
@@ -0,0 +1,49 @@
+package com.thoughtworks.springbootemployee.intergration;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CompanyIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    private void initCompany(){
+        Company company = new Company(1,"OOCL",1, Arrays.asList(
+                new Employee(1,"zach",18,"male",1000)
+        ));
+        companyRepository.save(company);
+    }
+
+    @Test
+    void should_return_companies__when_hit_get_all_companies_given_nothing() throws Exception {
+        //given
+        initCompany();
+
+        //when
+        mockMvc.perform(get("/companies"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",hasSize(1)))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].companyName").value("OOCL"))
+                .andExpect(jsonPath("$[0].employeesNumber").value(1))
+                .andExpect(jsonPath("$[0].employees",hasSize(1)));
+    }
+}
