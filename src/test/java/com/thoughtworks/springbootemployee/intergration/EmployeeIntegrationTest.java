3ca75161655c70c4e6adba5670b55496030f0aec
LISH5 <727904507@qq.com>
2020-07-30 15:59:33 +0800

    [RESTFUL API][ShaoLi,Zach] test: add should_return_employees_when_hit_get_employee_by_page_endpoint_given_page_and_page_size

--- src/test/java/com/thoughtworks/springbootemployee/intergration/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/intergration/EmployeeIntegrationTest.java
@@ -50,4 +50,28 @@ public class EmployeeIntegrationTest {
                 .andExpect(jsonPath("$[0].salary").value(500))
                 .andExpect(jsonPath("$[0].companyId").value(1));
     }
+
+    @Test
+    void should_return_employees_when_hit_get_employee_by_page_endpoint_given_page_and_page_size() throws Exception {
+        //given
+        Company company = new Company(1, "OOCL", 10000, Collections.emptyList());
+        Company savedCompany = companyRepository.save(company);
+        Employee employee = new Employee(1, "ShaoLi", 22, "male", 500);
+        employee.setCompanyId(1);
+        employeeRepository.save(employee);
+        int page = 1;
+        int pageSize = 1;
+
+        //when
+        mockMvc.perform(get("/employees?page=" + page + "&pageSize=" + pageSize))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content", hasSize(1)))
+                .andExpect(jsonPath("$.content[0].id").isNumber())
+                .andExpect(jsonPath("$.content[0].name").value("ShaoLi"))
+                .andExpect(jsonPath("$.content[0].age").value(22))
+                .andExpect(jsonPath("$.content[0].gender").value("male"))
+                .andExpect(jsonPath("$.content[0].salary").value(500))
+                .andExpect(jsonPath("$.content[0].companyId").value(1));
+    }
+
 }
