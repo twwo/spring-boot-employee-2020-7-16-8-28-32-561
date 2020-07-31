fd08ee23859efd18cb1b009a5c3b5a8b2f387c83
LISH5 <727904507@qq.com>
2020-07-31 10:03:47 +0800

    [RESTFUL API][ShaoLi] refactor: add LomBok annotation and refactor Company and Employee

--- src/main/java/com/thoughtworks/springbootemployee/model/Company.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -1,9 +1,16 @@
 package com.thoughtworks.springbootemployee.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Company {
 
     @Id
@@ -14,46 +21,4 @@ public class Company {
     @OneToMany(cascade = {CascadeType.ALL})
     @JoinColumn(name = "companyId")
     private List<Employee> employees;
-
-    public Company() {
-    }
-
-    public Company(Integer id, String companyName, Integer employeesNumber, List<Employee> employees) {
-        this.id = id;
-        this.companyName = companyName;
-        this.employeesNumber = employeesNumber;
-        this.employees = employees;
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public Integer getEmployeesNumber() {
-        return employeesNumber;
-    }
-
-    public void setEmployeesNumber(Integer employeesNumber) {
-        this.employeesNumber = employeesNumber;
-    }
-
-    public List<Employee> getEmployees() {
-        return employees;
-    }
-
-    public void setEmployees(List<Employee> employees) {
-        this.employees = employees;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
 }
